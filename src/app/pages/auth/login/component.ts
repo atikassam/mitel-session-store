346cf5a902965244c4dc9b2641a7e5877682d945
atik <atikassam@outlook.com>
2019-10-15 17:24:33 +0530

    login error is added

--- src/app/pages/auth/login/component.ts
+++ src/app/pages/auth/login/component.ts
@@ -1,5 +1,4 @@
 import {AfterViewInit, Component, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
-import {ItemListComponent} from "../../components/item-list/component";
 
 @Component({
   templateUrl: './component.html',
